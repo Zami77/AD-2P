e420233657431fb308a5c644ab2104c3599b310e
Daniel Lungaro <chunk_77@hotmail.com>
2022-02-12 10:42:27 -0800

    feat: read json and store in vuln map

--- AD-2P/AD2P.py
+++ AD-2P/AD2P.py
@@ -1,15 +1,15 @@
 import sys
 import os
+from Vulnerabilities2038 import vulnerabilities_2038_map as vuln_map
 from datetime import datetime
 from clang.cindex import Index, Cursor
-
 
 def get_all_nodes(node: Cursor, nodesList = []):
     [get_all_nodes(c, nodesList)
         for c in node.get_children()]
     
     # TODO: Check against JSON list of potential vulnerabilities
-    if node.spelling == 'time':
+    if node.spelling in vuln_map:
         nodesList.append({ 
             'kind' : node.kind,
             'spelling' : node.spelling,
@@ -28,13 +28,14 @@ def parse_nodes_from_file(filename: str):
 
 def main():
     if len(sys.argv) < 2:
-        print('invalid number arguments.\npython ', os.path.basename(__file__), ' filename')
+        print('invalid number of arguments.\nUsage:\tpython ', os.path.basename(__file__), ' filename')
         return -1
 
     # TODO: Check if directory, then recursively search
+    print(vuln_map.keys())
     filename = sys.argv[1]
     allNodes = parse_nodes_from_file(filename)
-    print(allNodes)
+    # print(allNodes)
 
 if __name__ == '__main__':
     main()
