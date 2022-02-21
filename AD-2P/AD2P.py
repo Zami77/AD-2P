ef038d5a9a081a0bb40d8ff36abc927da16a0519
Daniel Lungaro <chunk_77@hotmail.com>
2022-02-21 09:41:53 -0800

    cleaned up nodes

--- AD-2P/AD2P.py
+++ AD-2P/AD2P.py
@@ -1,6 +1,7 @@
 import sys
 import os
-from AD2PNode import AD2PNode, parse_location_from_node, AD2PNodeLocation
+from typing import List
+from AD2PNode import AD2PNode, AD2PNodeLocation
 from Vulnerabilities2038 import vulnerabilities_2038_map as vuln2038_map
 from clang.cindex import Index, Cursor
 
@@ -9,21 +10,13 @@ def get_all_nodes(node: Cursor, nodesList = []):
         for c in node.get_children()]
     
     if node.spelling in vuln2038_map:
-        # nodesList.append(AD2PNode(name=node.spelling,location=))
-        print(node.location)
-        nodesList.append({ 
-            'kind' : node.kind,
-            'spelling' : node.spelling,
-            'displayname' : node.displayname,
-            'type' : node.type,
-            'line' : node.location
-        })
+        nodesList.append(AD2PNode(name=node.spelling, location=AD2PNodeLocation(node.location), description=vuln2038_map[node.spelling].Description))
 
 def parse_nodes_from_file(filename: str):
     index = Index.create()
     translation_unit = index.parse(filename)
     print('Translation Unit', translation_unit.spelling)
-    allNodes: AD2PNode = []
+    allNodes: List[AD2PNode] = []
     get_all_nodes(translation_unit.cursor, allNodes)
     return allNodes
 
@@ -35,7 +28,8 @@ def main():
     # TODO: Check if directory, then recursively search
     filename = sys.argv[1]
     allNodes = parse_nodes_from_file(filename)
-    print(allNodes)
+    for node in allNodes:
+        print(node)
 
 if __name__ == '__main__':
     main()
--- AD-2P/AD2PNode.py
+++ AD-2P/AD2PNode.py
@@ -1,18 +1,19 @@
-class AD2PNodeLocation:
-    def __init__(self, sourceFile = '', line: int = 0, column: int = 0) -> None:
-        self.SourceFile = sourceFile
-        self.Line = line
-        self.Column = column
+from clang.cindex import File, SourceLocation
 
-@staticmethod
-def parse_location_from_node(node_location) -> AD2PNodeLocation:
-    pass
+class AD2PNodeLocation:
+    def __init__(self, sourceFile: SourceLocation = SourceLocation()):
+        if sourceFile.file is None:
+            self.SourceFile = 'Unknown'
+        else:
+            self.SourceFile = sourceFile.file.name
+        self.Line = sourceFile.line
+        self.Column = sourceFile.column
 
 class AD2PNode:
-    def __init__(self, name = '', location: AD2PNodeLocation = AD2PNodeLocation(), description = '') -> None:
+    def __init__(self, name: str = '', location: AD2PNodeLocation = AD2PNodeLocation(), description: str = '') -> None:
         self.Name = name
         self.Location = location
         self.Description = description
     
-    def print_violation(self) -> str:
-        return 'Violation Found:' + self.Name + ' at line ' + self.Location.Line + ', column ' + self.Location.Column + ' of ' + self.Location.SourceFile
+    def __str__(self) -> str:
+        return '|Violation Found| Name: %-25s line: %-10d column: %-10d Source: %-300s' % (self.Name, self.Location.Line, self.Location.Column, self.Location.SourceFile)
