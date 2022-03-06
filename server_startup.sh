8a6548d476aeea4f8ff928419346e6097ca38788
Daniel Lungaro <chunk_77@hotmail.com>
2022-03-06 13:00:09 -0800

    change server startup shell

--- server_startup.sh
+++ server_startup.sh
@@ -1,2 +1,1 @@
-pip install -r requirementsWebServer.txt
-gunicorn AD2PApi:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80
+gunicorn AD2PApi:app --workers 4 --worker-class uvicorn.workers.UvicornWorker
