5f88a27684a0c6c6061a32725e4d5ad362e048f1
Daniel Lungaro <chunk_77@hotmail.com>
2022-03-05 20:51:48 -0800

    feat: functioning ad2p scan endpoint

--- AD-2P/AD2PApi.py
+++ AD-2P/AD2PApi.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, UploadFile
 from AD2PHelper import valid_files
-from AD2P import main
+from AD2P import main as AD2PMain
 
 app = FastAPI()
 
@@ -14,10 +14,17 @@ async def get_valid_files():
         "valid_files": valid_files
     }
 
+def write_file_to_scanned_files(file: UploadFile, file_path: str):
+    with open(file_path, "wb+") as file_object:
+            file_object.write(file.file.read())
+
 @app.post("/AD2PScan")
 async def ad2p_scan(file: UploadFile):
-    # TODO: have to save file locally and send that path to AD2P
+    file_path = f"scanned_files/{file.filename}"
+    write_file_to_scanned_files(file, file_path)
+    scan_result = AD2PMain(file_path)
+    
     return {
         "filename": file.filename,
-        "scan_result": main(file.filename)
+        "scan_result": scan_result
     }
